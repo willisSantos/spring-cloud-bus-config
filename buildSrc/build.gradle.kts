eb22064d07e03a9682f3ed69abbc4c8ecbe6ba5a
Willis Vieira Santos Junior <willisjunior89@gmail.com>
2024-08-21 16:04:06 -0300

    adding toml file for configuration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.3.2")
-    implementation("org.graalvm.buildtools:native-gradle-plugin:0.10.2")
+    implementation(libs.spring.boot.plugin)
+    implementation(libs.graalvm.buildtools)
 }
 
 repositories {
--- buildSrc/src/main/kotlin/online/willissantos.springcloudbusconfig/java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/online/willissantos.springcloudbusconfig/java-conventions.gradle.kts
@@ -11,17 +11,16 @@ repositories {
 
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:3.3.2"))
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
-
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+//TODO search for clean ways to organize
+val catalogs = extensions.getByType<VersionCatalogsExtension>()
+val libs = catalogs.named("libs")
+dependencies.addProvider("compileOnly", libs.findLibrary("lombok").get())
+dependencies.addProvider("annotationProcessor", libs.findLibrary("lombok").get())
 
 configurations {
     compileOnly {
