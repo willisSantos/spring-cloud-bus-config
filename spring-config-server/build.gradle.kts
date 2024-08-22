c3784acd32a29275c2ec1241ddeeddb1ba1cb608
Willis Vieira Santos Junior <willisjunior89@gmail.com>
2024-08-22 18:15:13 -0300

    Adding toolchains into project

--- spring-config-server/build.gradle.kts
+++ spring-config-server/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("java-conventions")
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-config-server")
 }
--- spring-rest-api/build.gradle.kts
+++ spring-rest-api/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("java-conventions")
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
